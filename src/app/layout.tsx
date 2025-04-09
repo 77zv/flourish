a4eb16fcdcf250f6ba7b7df7ef1117b317f5133f
Antony <antonylbrussolo@gmail.com>
2025-04-09 15:40:58 -0400

    Add Header and Footer components to layout for improved site structure and navigation. Include Font Awesome stylesheet for enhanced icon usage.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "./_components/Nav";
+import Footer from "./_components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,7 +16,8 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Flourish Institute - Tutoring Services",
-  description: "We turn the dreams of our students into a reality. Join our tutoring programs and excel in your studies.",
+  description:
+    "We turn the dreams of our students into a reality. Join our tutoring programs and excel in your studies.",
 };
 
 export default function RootLayout({
@@ -24,10 +27,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+          integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Header />
+        <main className="pt-16">{children}</main>
+        <Footer />
       </body>
     </html>
   );
