01099f5fe4f77debd4d054ce907f6cfac0e135f7
Antony <antonylbrussolo@gmail.com>
2025-04-10 17:21:09 -0400

    Add constants for professions, grade levels, and exams in a new constants.ts file to standardize application data and enhance maintainability.

---
+++ src/app/_components/constants.ts
@@ -0,0 +1,61 @@
+export const PROFESSIONS = [
+  "Doctor",
+  "Engineer",
+  "Lawyer",
+  "Banker",
+  "Entrepreneur",
+];
+
+export const gradeLevels = [
+  {
+    iconType: 'child',
+    label: "Elementary (K–5)",
+    title: "Building Strong Foundations",
+    description: "Engaging online classes that nurture fundamental skills in reading, math, and creative exploration.",
+  },
+  {
+    iconType: 'reader',
+    label: "Middle (6–8)",
+    title: "Developing Critical Thinkers",
+    description: "Project-based learning and interactive lessons to encourage analytical skills and intellectual curiosity.",
+  },
+  {
+    iconType: 'gradCap',
+    label: "Secondary (9–12)",
+    title: "Unlocking Academic Excellence",
+    description: "Advanced curricula, competitive exam prep, and mentorship to help students excel.",
+  },
+  {
+    iconType: 'university',
+    label: "Post-Secondary",
+    title: "Elevating Career Paths",
+    description: "Specialized tutoring, graduate exam prep, and real-world guidance for college and beyond.",
+  },
+];
+
+export const exams = [
+  {
+    iconType: 'award',
+    label: "IB & AP",
+    title: "Achieve Outstanding Scores",
+    description: "Comprehensive online courses to master advanced placement material, led by experienced IB & AP mentors.",
+  },
+  {
+    iconType: 'trophy',
+    label: "SAT & Competitions",
+    title: "Compete Nationally & Globally",
+    description: "Intensive strategy sessions to boost SAT scores, plus preparation for Olympiads and other contests.",
+  },
+  {
+    iconType: 'brain',
+    label: "MCAT & LSAT",
+    title: "Master Professional Exams",
+    description: "Tailored study plans for aspiring med and law students, featuring real-case analyses and advanced content.",
+  },
+  {
+    iconType: 'gradCap',
+    label: "GRE & GMAT",
+    title: "Excel in Graduate Admissions",
+    description: "Data-driven approach to tackle quantitative, verbal, and analytical writing sections with confidence.",
+  },
+]; 
